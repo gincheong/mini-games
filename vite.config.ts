749e0b6cd2b15c7aedb881bf4877962d35cca635
gincheong2 <gincheong2@gmail.com>
2026-06-07 15:27:14 +0900

    chore: remove tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import netlify from '@netlify/vite-plugin-tanstack-start';
+import { devtools } from '@tanstack/devtools-vite';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), netlify(), tailwindcss(), tanstackStart(), viteReact()],
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [devtools(), netlify(), tanstackStart(), viteReact()],
+});
 
-export default config
+export default config;
